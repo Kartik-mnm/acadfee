c092377a4693ffe0bd892a7587c1689ed4795a46
Kartik-mnm <aspirantth@gmail.com>
2026-03-12 20:07:15 +0530

    fix backend url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://acadfee-api.onrender.com/api",
+ baseURL: "https://acadfee.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
