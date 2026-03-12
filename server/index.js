4842e5657a08f8dfd9c3274daac87c493173ffcf
Kartik-mnm <aspirantth@gmail.com>
2026-03-12 19:53:05 +0530

    fix cors

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || "*" }));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // Routes
